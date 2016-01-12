1dc85212fa3a950cbbabf43b30ff6366b75c47f9
duzhuo <duzhuoloveyou@163.com>
2016-01-12 22:42:07 +0800

    declare-parents

---
+++ src/main/java/com/springincation/springidol/Contestant.java
@@ -0,0 +1,8 @@
+package com.springincation.springidol;
+
+/**
+ * Created by MrDu on 16/1/12.
+ */
+public interface Contestant {
+    void receiveAward();
+}
---
+++ src/main/java/com/springincation/springidol/GraciousContestant.java
@@ -0,0 +1,10 @@
+package com.springincation.springidol;
+
+/**
+ * Created by MrDu on 16/1/12.
+ */
+public class GraciousContestant implements Contestant {
+    public void receiveAward() {
+        System.out.println("receiveAward receiveAward receiveAward");
+    }
+}
